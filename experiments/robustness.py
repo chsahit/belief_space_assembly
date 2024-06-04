c130c581ff738ef50501cd7f11873c91795a82ec
Sahit Chintalapudi <chsahit@gmail.com>
2024-06-04 10:42:48 -0400

    nondaemonic multiprocess of b-est experiments

--- experiments/robustness.py
+++ experiments/robustness.py
@@ -1,7 +1,10 @@
 import logging
+import multiprocessing
+import multiprocessing.pool
 
 from tqdm import tqdm
 
+import components
 import contact_defs
 import dynamics
 import state
@@ -17,12 +20,54 @@ planners = {"cobs": cobs.cobs, "b_est": ao_b_est.b_est, "no_k": cobs.no_k}
 logging.basicConfig(level=logging.WARNING)
 
 
+# https://stackoverflow.com/questions/52948447/error-group-argument-must-be-none-for-now-in-multiprocessing-pool
+class NonDaemonPool(multiprocessing.pool.Pool):
+    def Process(self, *args, **kwds):
+        proc = super(NonDaemonPool, self).Process(*args, **kwds)
+
+        class NonDaemonProcess(proc.__class__):
+            """Monkey-patch process to ensure it is never daemonized"""
+
+            @property
+            def daemon(self):
+                return False
+
+            @daemon.setter
+            def daemon(self, val):
+                pass
+
+        proc.__class__ = NonDaemonProcess
+        return proc
+
+
 def make_b0(peg_size: str) -> state.Belief:
     urdf = peg_urdfs[peg_size]
     p0 = init_particle.init_peg(X_GM_x=-0.01, peg_urdf=urdf)
     p1 = init_particle.init_peg(pitch=0, peg_urdf=urdf)
     p2 = init_particle.init_peg(X_GM_x=0.01, peg_urdf=urdf)
     return state.Belief([p0, p1, p2])
+
+
+def run_test(planner_name: str, goal: components.ContactState):
+    b0 = make_b0("normal")
+    plan_result = planners[planner_name](b0, goal)
+    b0_test = make_b0("big")
+    bT_test = dynamics.sequential_f_bel(b0_test, plan_result.traj)[-1]
+    if bT_test.satisfies_contact(goal):
+        print("return 1.0")
+        return 1.0
+    print("return 0.0")
+    return 0.0
+
+
+def parallel_sweep(planner_name: str, trials: int = 25):
+    p = NonDaemonPool(8)
+    arg_list = [(planner_name, contact_defs.bottom_faces_2) for i in range(trials)]
+    scores = p.starmap(run_test, arg_list)
+    p.close()
+    p.join()
+    sr = float(scores) / float(len(trials))
+    print(f"{planner_name}, {sr}")
 
 
 def sweep_geoms(planner_name: str):
@@ -50,5 +95,4 @@ def sweep_geoms(planner_name: str):
 
 
 if __name__ == "__main__":
-    sweep_geoms("cobs")
-    sweep_geoms("b_est")
+    parallel_sweep("b_est")
